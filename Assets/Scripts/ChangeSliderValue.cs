85de67d2dbc9ab32ed5e15e7e62ee8fd420ba7ee
Victor Kudelin <58315363+K-vic98@users.noreply.github.com>
2020-12-25 07:45:07 +0300

    Made health bar

---
+++ Assets/Scripts/ChangeSliderValue.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ChangeSliderValue : MonoBehaviour
+{
+    [SerializeField] float _numberOfDamagePoints;
+    [SerializeField] float _numberOfHealingPoints;
+
+    private Slider _healthBar;
+
+    private void Awake()
+    {
+        _healthBar = GetComponent<Slider>();
+    }
+
+    public void Heal()
+    {
+        _healthBar.DOValue(_healthBar.value + _numberOfHealingPoints, 1);
+    }
+
+    public void Hit()
+    {
+        _healthBar.DOValue(_healthBar.value - _numberOfDamagePoints, 1);
+    }
+}
